be6ee11bff33475d4aafaea50023ed03ff08c627
Brice Saussay <brice.saussay@sbg-systems.com>
2020-10-20 14:52:28 +0200

    Revert "sbg: Add handleInjectDataTopic to gps helper"
    This reverts commit 851befd4495ca1db092fa0c60d4ff63b61678950.

--- src/drivers/gps/gps.cpp
+++ src/drivers/gps/gps.cpp
@@ -338,10 +338,6 @@ int GPS::callback(GPSCallbackType type, void *data1, int data2, void *user)
 	case GPSCallbackType::setClock:
 		px4_clock_settime(CLOCK_REALTIME, (timespec *)data1);
 		break;
-
-	case GPSCallbackType::handleInjectDataTopic:
-		gps->handleInjectDataTopic();
-		break;
 	}
 
 	return 0;
