7093cd6193caf4a9e77837fd01f432069bd45e91
Brice Saussay <brice.saussay@sbg-systems.com>
2020-10-21 12:31:30 +0200

    sbg: Add sbgECom library

---
+++ src/lib/sbgECom/src/binaryLogs/sbgEComBinaryLogDiag.c
@@ -0,0 +1,50 @@
+// sbgCommonLib headers
+#include <sbgECom/common/sbgCommon.h>
+#include <sbgECom/common/streamBuffer/sbgStreamBuffer.h>
+
+// Local headers
+#include "sbgEComBinaryLogDiag.h"
+
+//----------------------------------------------------------------------//
+//- Public functions                                                   -//
+//----------------------------------------------------------------------//
+
+SbgErrorCode sbgEComBinaryLogParseDiagData(SbgStreamBuffer *pInputStream, SbgLogDiagData *pOutputData)
+{
+	uint8_t cast;
+	assert(pInputStream);
+	assert(pOutputData);
+
+	pOutputData->timestamp		= sbgStreamBufferReadUint32LE(pInputStream);
+	pOutputData->type			= (SbgDebugLogType) (cast = sbgStreamBufferReadUint8(pInputStream));
+	pOutputData->errorCode		= (SbgErrorCode)    (cast = sbgStreamBufferReadUint8(pInputStream));
+
+	sbgStreamBufferReadBuffer(pInputStream, pOutputData->string, sbgStreamBufferGetSpace(pInputStream));
+	pOutputData->string[sizeof(pOutputData->string) - 1] = '\0';
+
+	return sbgStreamBufferGetLastError(pInputStream);
+}
+
+SbgErrorCode sbgEComBinaryLogWriteDiagData(SbgStreamBuffer *pOutputStream, const SbgLogDiagData *pInputData)
+{
+	size_t								 length;
+
+	assert(pOutputStream);
+	assert(pInputData);
+
+	sbgStreamBufferWriteUint32LE(pOutputStream, pInputData->timestamp);
+	sbgStreamBufferWriteUint8(pOutputStream, pInputData->type);
+	sbgStreamBufferWriteUint8(pOutputStream, pInputData->errorCode);
+
+	length = strlen(pInputData->string);
+
+	if (length >= sizeof(pInputData->string))
+	{
+		length = sizeof(pInputData->string) - 1;
+	}
+
+	sbgStreamBufferWriteBuffer(pOutputStream, pInputData->string, length);
+	sbgStreamBufferWriteUint8(pOutputStream, 0);
+
+	return sbgStreamBufferGetLastError(pOutputStream);
+}
